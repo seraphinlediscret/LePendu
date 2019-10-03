631dbbe08c45dc7feb6674596550a77110dd31a8
Yohann <yohann.greving@laposte.net>
2019-10-03 09:31:16 +0200

    fin et en anglais

--- JS avancé/pendu/index.js
+++ JS avancé/pendu/index.js
@@ -1,57 +1,60 @@
 //////////////////variables//////////////////
 let play = document.getElementById("btn");
-let button = document.getElementsByClassName("lettre");
-let affichage = document.getElementById("text");
+let button = document.getElementsByClassName("letter");
+let screen = document.getElementById("text");
 let canvas = document.getElementById("cnv");
 let ctx = canvas.getContext('2d');
 let start = document.getElementById("btn");
 let underscore = [];
 let table = ["orchidoclaste","bacchanales","hemistiche","incrementation","acropole","parnassien","hetaires","hecatonchires","metaphysique","aristotelicien"];
-let reponse;
+let response;
 let box = document.querySelector("text");
 let count = 0;
+let reset = document.getElementById("reset");
 
-
-//////////////////démarrer partie/////////////////////////
-function leCanvas() {
+//////////////////start party/////////////////////////
+function TheCanvas() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);   
 }
 
-///////////////affichage et remplacement des lettres////////////////////////////////
+///////////////word in underscore////////////////////////////////
 
-function LesMots() {
-    reponse = table[Math.floor(Math.random() * table.length)];
-    box = reponse.split('');//lettres des mots
-    leCanvas();
-    affichage.textContent = "";
+function WordsBoys() {
+    
+    response = table[Math.floor(Math.random() * table.length)];
+    box = response.split('');
+    TheCanvas();
+    screen.textContent = "";
     console.log(box);
     
     for (let i = 0; i < box.length; i++) {
-         affichage.textContent += "_";  
+         screen.textContent += "_";  
     }
-    underscore = affichage.textContent.split("");
+    underscore = screen.textContent.split("");
     console.log(underscore);
+    button.style.backgroundColor = "gray";
 }
-start.addEventListener("click", LesMots);
+start.addEventListener("click", WordsBoys);
 
-////////////// reception des touches///////////////
-function buttonColor(lettre) {
-            lettre.style.width = "50px";
+////////////// receiving keys ///////////////
+
+function buttonColor(letter) {
+            letter.style.width = "50px";
        }
 
-function letter(){
+function Letters(){
        console.log(this.value)
 }
 
 for (let i = 0; i < button.length; i++) {
-    button[i].addEventListener("click", letter);
+    button[i].addEventListener("click", Letters);
 }
-
-      
-
-////////////////lettres trouvées dans le mot////////////////////
+function Act(){
+    window.location.reload();
+}
+////////////////letter find////////////////////
 function verify(){
-    let lettreTrouvee = false ;
+    let letterFind = false ;
 
     for (let i = 0; i < box.length; i++) {
         console.log(this.value+" button")
@@ -60,53 +63,52 @@ function verify(){
            console.log(box[i]+" box")
            underscore[i] = this.value;
            console.log(underscore[i] +" underscore");
-           affichage.textContent = underscore.join(""); 
-           lettreTrouvee = true;
+           screen.textContent = underscore.join(""); 
+           letterFind = true;
            this.style.backgroundColor = "green";
         }    
         }
-        if(affichage.textContent == reponse){
-          alert("Gagné! Bravooo ! La puissance est en toi !!!!!!!!");
-           window.location.reload();
-            
+        if(screen.textContent == response){
+          alert("Gagné! Bravo ! Tu feras quelque chose de ta vie finalement !");
+           window.location.reload();   
     }
-    if(!lettreTrouvee)
+    if(!letterFind)
         {
            count++;
            console.log(count);
            this.style.backgroundColor = "red";
         }
       
-        ////////////////////////////////////dessin////////////////////////
-//pied
+        ////////////////////////////////////drawn////////////////////////
+//foot
 if(count == 1){ 
     ctx.lineWidth=2;
     ctx.beginPath(); 
     ctx.moveTo(10,600);
     ctx.lineTo(100,500);
     ctx.lineTo(200,600);
-    //poteau
+    //post
     }else if(count == 2){
     ctx.moveTo(100,500); 
     ctx.lineTo(100,100);
-    //haut
+    //top
     }else if(count == 3){
     ctx.moveTo(100,100); 
     ctx.lineTo(500,100);
-    //corde
+    //rope
     } else if(count == 4){
     ctx.moveTo(500,100); 
     ctx.lineTo(500,200);
-    //tête
+    //head
     } else if(count == 5){
     ctx.moveTo(500,200); 
     ctx.arc(500,300, 40,0, 2*Math.PI, true);
-    //cou et corps
+    //neck and body
     } else if(count == 6){
     ctx.moveTo(530,300); 
     ctx.lineTo(540,330);
     ctx.lineTo(540,400);
-    //bras et jambes
+    //arms and legs
     } else if(count == 7){
     ctx.moveTo(510,360); 
     ctx.lineTo(540,330);
@@ -118,15 +120,14 @@ if(count == 1){
     ctx.stroke();
 
     if(count > 6){
-        alert("T'es mort booouuuh ! le mot était " + "[" + reponse + "]");
+        alert("T'es mort booouuuh ! le mot était " + "[" + response + "]");
         window.location.reload();
-    }
-    
-       
+    }    
 }
 for (let i = 0; i < button.length; i++) {
     button[i].addEventListener("click", verify);
 }
-////////////////// dessin progressif/////////////////
+reset.addEventListener("click", Act)
+
 
 
